90de83957e4c4bc097d5c78cca23db23a639abef
WeijieChen2017 <weijiechen1994@gmail.com>
2018-12-31 22:13:07 -0600

    regularizer

--- GridSearch_reg.py
+++ GridSearch_reg.py
@@ -4,11 +4,12 @@
 import os
 
 static = 'python Helium.py --flag_reg True -e 700 --type_wr l2 --type_yr l2'
-for idx_yr in range(6):
-    for idx_wr in range(6):
+for idx_yr in range(9):
+    for idx_wr in range(9):
         yr = '1e-'+str(idx_yr+1)
         wr = '1e-'+str(idx_wr+1)
-        ID = ' --para_yr '+yr+' --para_wr '+wr+' --id yr'+str(idx_yr+1)+'_wr'+str(idx_wr+1)+'_l2'
-        static_curr = static+ID
-        print(static_curr)
-        os.system(static_curr)
+        if (yr>6)or(wr>6):
+            ID = ' --para_yr '+yr+' --para_wr '+wr+' --id yr'+str(idx_yr+1)+'_wr'+str(idx_wr+1)+'_l2'
+            static_curr = static+ID
+            print(static_curr)
+            os.system(static_curr)
