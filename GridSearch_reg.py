bd0678d40f168f9eaad1ed4c6fba8c4ea6fe8ea3
WeijieChen2017 <weijiechen1994@gmail.com>
2018-12-31 18:30:07 -0600

    regularizer

--- GridSearch_reg.py
+++ GridSearch_reg.py
@@ -8,7 +8,7 @@ for idx_yr in range(6):
     for idx_wr in range(6):
         yr = '1e-'+str(idx_yr+1)
         wr = '1e-'+str(idx_wr+1)
-        ID = ' --para_yr '+yr+'--para_wr '+wr+' --id yr'+str(idx_yr+1)+'_wr'+str(idx_wr+1)+'_l2'
+        ID = ' --para_yr '+yr+' --para_wr '+wr+' --id yr'+str(idx_yr+1)+'_wr'+str(idx_wr+1)+'_l2'
         static_curr = static+ID
         print(static_curr)
         os.system(static_curr)
