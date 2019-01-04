f314da487c2b887922e2543b368142767b9741ba
WeijieChen2017 <weijiechen1994@gmail.com>
2019-01-03 22:19:01 -0600

    factor of norm

--- data/set_X_Y.py
+++ data/set_X_Y.py
@@ -11,15 +11,15 @@ def data_pre_PVC(data_mri, data_pet):
     IMG_ROWS = GL_get_value("IMG_ROWS")
     IMG_COLS = GL_get_value("IMG_COLS")
     IDX_SLICE = GL_get_value("IDX_SLICE")
-    FA_NORM = GL_get_value("FA_NORM")
+    # FA_NORM = GL_get_value("FA_NORM")
+    FA_NORM = np.amax(data_pet[:, :, IDX_SLICE])
     MRI_TH = float(GL_get_value("MRI_TH"))
 
     X = np.zeros((1, IMG_ROWS, IMG_COLS, 4))
     Y = np.zeros((1, IMG_ROWS, IMG_COLS, 4))
     Z = np.zeros((1, IMG_ROWS, IMG_COLS, 2))
 
-    # data_pet = np.divide(data_pet, FA_NORM)
-    data_pet = np.divide(data_pet, np.amax(X))
+    data_pet = np.divide(data_pet, FA_NORM)
 
     Z[0, :, :, 0] = data_pet[:, :, IDX_SLICE] <= MRI_TH
     Z[0, :, :, 1] = data_mri[:, :, IDX_SLICE] == 3
