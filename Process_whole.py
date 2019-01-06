17f3f0710aabe6e0066159287b0ac66de5c3b8b1
WeijieChen2017 <weijiechen1994@gmail.com>
2019-01-05 20:43:12 -0600

    full image

--- Process_whole.py
+++ Process_whole.py
@@ -3,8 +3,10 @@
 
 import os
 
-static = 'python Helium.py --w 1110 --n_filter 64 --epoch 40 --id d4f64PET --flag_whole True --idx_slice '
+static = 'python Helium.py --w 1110 --n_filter 64 --epoch 40 --id d4f64eeVee' \
+         ' --flag_whole True --flag_reg True --type_yr l2 --para_yr 1e-9 ' \
+         '--idx_slice '
 for idx in range(284):
     static_curr = static+str(idx)
     print(static_curr)
-    os.system(static_curr)
+    # os.system(static_curr)
