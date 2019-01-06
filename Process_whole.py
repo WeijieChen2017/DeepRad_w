6f37b11b959326bbdc7cf3621b333b14cf56f4c3
WeijieChen2017 <weijiechen1994@gmail.com>
2019-01-06 00:17:35 -0600

    ridge

--- Process_whole.py
+++ Process_whole.py
@@ -3,15 +3,11 @@
 
 import os
 
-static = 'python Helium.py --w 1110 --n_filter 64 --epoch 40 --id d4f64eeVee' \
+static = 'python Helium.py --w 1110 --n_filter 64 --epoch 40 --id d4f64_Kirby' \
          ' --flag_whole True --flag_reg True --type_yr l2 --para_yr 1e-9 ' \
          '--idx_slice '
-# for idx in range(284):
-#     static_curr = static+str(idx)
-#     print(static_curr)
-#     os.system(static_curr)
 
-idx = 142
-static_curr = static+str(idx)
-print(static_curr)
-os.system(static_curr)
+for idx in range(284):
+    static_curr = static+str(idx)
+    print(static_curr)
+    os.system(static_curr)
